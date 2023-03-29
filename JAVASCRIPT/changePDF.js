809a3cdc58ba3c016fef25a0329c28136fde2ca9
KevenWM <keven.mendes@venetoinvest.com.br>
2023-03-29 17:49:36 -0300

    Add Audios empresas - 29/03/2023

--- JAVASCRIPT/changePDF.js
+++ JAVASCRIPT/changePDF.js
@@ -2,12 +2,15 @@
 function searchPDF() {
     const pdfNameInput = document.getElementById('fruitInput');
     const pdfViewer = document.getElementById('pdfViewer');
+    const audio = document.getElementById('tese-audio');
 
 
     const pdfName = pdfNameInput.value.trim(); // Trim whitespace from input value
     if (pdfName !== '') { // Check that input value is not empty
         const pdfPath = `../PDF/${pdfName}.pdf`;
+        const audioPath = `../AUDIO/${pdfName}.m4a`;
         pdfViewer.src = pdfPath;
+        audio.src = audioPath
     }
 }
 
